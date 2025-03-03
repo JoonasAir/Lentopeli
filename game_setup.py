85ba54b29e38761ea06be4077e0aef66ae4d4b15
Eeli Kiuru <eeli.kiuru@metropolia.fi>
2025-03-03 19:52:04 +0200

    korjaukset

--- game_setup.py
+++ game_setup.py
@@ -16,8 +16,7 @@ def game_setup():
     difficulty_settings = {
         'E': { # Easy
             'game_money': 5000,         # money at the beginning of the game
-            'game_time': 60*5,          # money at the beginning of the game
-            'mistakes_allowed': 3,      # allowed amount of mistakes during a game
+            'game_time': 60*5,          # time at the beginning of the game
             'random_luck': 0.05,        # the possibility of benefiting from random functions
             'criminal_headstart': 2,    # the criminal's head start at the start of the game
             'criminal_time': 60,        # the time interval at which the criminal flies to the next location
@@ -26,7 +25,6 @@ def game_setup():
         'N': { # Normal
             'game_money': 3500,         # money at the beginning of the game
             'game_time': 60*4,          # time at the beginning of the game
-            'mistakes_allowed': 2,      # allowed amount of mistakes during a game
             'random_luck': 0.025,       # the possibility of benefiting from random functions
             'criminal_headstart': 3,    # the criminal's head start at the start of the game
             'criminal_time': 45,        # the time interval at which the criminal flies to the next location
@@ -34,8 +32,7 @@ def game_setup():
         },
         'H': { # Hard
             'game_money': 2500,         # money at the beginning of the game
-            'game_time': 60*3,          # money at the beginning of the game
-            'mistakes_allowed': 0,      # allowed amount of mistakes during a game
+            'game_time': 60*3,          # time at the beginning of the game
             'random_luck': 0.01,        # the possibility of benefiting from random functions
             'criminal_headstart': 4,    # the criminal's head start at the start of the game
             'criminal_time': 30,        # the time interval at which the criminal flies to the next location
@@ -43,8 +40,7 @@ def game_setup():
         },
         'X': { # For testing purposes. Feel free to adjust during testing. This will be removed from actual game.
             'game_money': 5000,         # money at the beginning of the game
-            'game_time': 60*5,          # money at the beginning of the game
-            'mistakes_allowed': 3,      # allowed amount of mistakes during a game
+            'game_time': 60*5,          # time at the beginning of the game
             'random_luck': 0.05,        # the possibility of benefiting from random functions
             'criminal_headstart': 4,    # the criminal's head start at the start of the game
             'criminal_time': 3,         # the time interval at which the criminal flies to the next location
