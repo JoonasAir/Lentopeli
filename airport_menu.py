69d45556e8947c660b447e85c0411ee6404f0252
Eeli Kiuru <eeli.kiuru@metropolia.fi>
2025-03-04 21:12:05 +0200

    edit

--- airport_menu.py
+++ airport_menu.py
@@ -1,36 +1,66 @@
-#   1. Talk to the security chief
-#           (find out if the criminal has been at the airport)
+from random import randint
 
 
-#   2. Visit the restroom
-#           (chance to meet an eyewitness. 
-#           The eyewitness might also deceive you)
+# you're in X country at X airport
+# t - time
+
+def airport_menu(game_dict:dict):
+    airport_actions = [
+        "Check remaining money",
+        "Talk to airport's security chief", 
+        "Solve the clue",                       # visible if we've talked to security chief and the criminal has been at the airport
+        "Try to solve the previous clue again", # visible if we gave wrong ansver in quiz -> we came to wrong airport and we have talked to security chief to find out that the criminal has not been here
+        "Buy a flight ticket",                  # visible after solving a clue
+        ]
+    random_actions = [
+        ("Visit the restroom",      "you got lucky",    "you went to restroom and feel better"),
+        ("Go to get a cup coffee",  "you got lucky",    "you went to restroom and feel better"),
+        ("Go to get a meal",        "you got lucky",    "you went to restroom and feel better"),
+        ("Go to relax in a lounge", "you got lucky",    "you went to restroom and feel better"),
+        ]
+    random_index = randint(0, len(random_actions)-1)
+    menu = "\nAIRPORT MENU\n\nChoose your action from following options:\n"
+    menu += f"    1 - {airport_actions[0]}\n"               # Check remaining time and money
+    menu += f"    2 - {random_actions[random_index][0]}\n"  # Randomly picked action from random_actions
+    menu += f"    3 - {airport_actions[1]}\n"               # Talk to airport's security chief
+
+# visible if we've talked to security chief and the criminal has been at the airport
+    menu += f"    4 - {airport_actions[2]}\n"               # Solve the clue
+
+# visible if we gave wrong ansver in quiz -> we came to wrong airport and we have talked to security chief to find out that the criminal has not been here
+    menu += f"    4 - {airport_actions[3]}\n"               # Try to solve the previous clue again
+
+# visible after solving a clue
+    menu += f"    4 - {airport_actions[4]}\n"               # Buy a flight ticket
 
 
-#   3. Have a coffee
-#           (chance to meet an eyewitness. 
-#           The eyewitness might also deceive you)
+    user_input = input(menu + "Input: ")
 
+    return user_input
 
-#   4. Have a meal
-#           (chance to meet an eyewitness. 
-#           The eyewitness might also deceive you)
+def airport_actions(user_input):
+    while user_input != 0:
+    
+        if user_input == 1 : # Check remaining time and money
+            print("Check remaining time and money")
+    
+        elif user_input == 2: # Randomly picked action from random_actions
+            print("Randomly picked action from random_actions")
 
+        elif user_input == 3: # Talk to airport's security chief
+            print("Talk to airport's security chief")
+    
+        elif user_input == 4: # Solve the clue
+            print("Solve the clue")
+    
+        elif user_input == 4: # Try to solve the previous clue again
+            print("Try to solve the previous clue again")
+    
+        elif user_input == 4: # Buy a flight ticket
+            print("Buy a flight ticket")
+            
+    
+    
 
-#   5. Buy a flight ticket
-#           (we can buy a flight ticket using the ICAO code)
-
-
-#   7. Check remaining time and money
-
-
-#  *8. Solve the clue --> quiz questions
-#           (this option is visible to the player if they have 
-#           talked to the security chief and if the criminal 
-#           has been at the airport)
-
-
-#  *8. Try to solve the previous clue again --> same quiz question as earlier
-#           (this option is visible to the player if they have 
-#           talked to the security chief and if the criminal 
-#           has not been at the airport)
+if __name__ == "__main__":
+    airport_actions(airport_menu())
--- game_setup.py
+++ game_setup.py
@@ -15,7 +15,10 @@ def game_setup():
             'random_luck': 0.05,        # the possibility of benefiting from random functions
             'criminal_headstart': 2,    # the criminal's head start at the start of the game
             'criminal_time': 60,        # the time interval at which the criminal flies to the next location
-            'difficulty': 'easy'        # difficulty for quiz questions
+            'difficulty': 'easy',       # difficulty for quiz questions
+            'talk_to_chief': False,     # have we talked to security chief at current airport or not? True=yes, False=no
+            'previous_quiz_answer':bool,# did we answer right or wrong on previous quiz question? True=right, False=wrong
+            'clue_solved': False        # have we solved a clue at current airport? True=yes, False=no
         },
         'N': { # Normal
             'game_money': 3500,         # money at the beginning of the game
@@ -24,7 +27,10 @@ def game_setup():
             'random_luck': 0.025,       # the possibility of benefiting from random functions
             'criminal_headstart': 3,    # the criminal's head start at the start of the game
             'criminal_time': 45,        # the time interval at which the criminal flies to the next location
-            'difficulty': 'medium'      # difficulty for quiz questions
+            'difficulty': 'medium',       # difficulty for quiz questions
+            'talk_to_chief': False,     # have we talked to security chief at current airport or not? True=yes, False=no
+            'previous_quiz_answer':bool,# did we answer right or wrong on previous quiz question? True=right, False=wrong
+            'clue_solved': False        # have we solved a clue at current airport? True=yes, False=no
         },
         'H': { # Hard
             'game_money': 2500,         # money at the beginning of the game
@@ -33,7 +39,10 @@ def game_setup():
             'random_luck': 0.01,        # the possibility of benefiting from random functions
             'criminal_headstart': 4,    # the criminal's head start at the start of the game
             'criminal_time': 30,        # the time interval at which the criminal flies to the next location
-            'difficulty': 'hard'        # difficulty for quiz questions
+            'difficulty': 'hard',       # difficulty for quiz questions
+            'talk_to_chief': False,     # have we talked to security chief at current airport or not? True=yes, False=no
+            'previous_quiz_answer':bool,# did we answer right or wrong on previous quiz question? True=right, False=wrong
+            'clue_solved': False        # have we solved a clue at current airport? True=yes, False=no
         },
         'X': { # For testing purposes. Feel free to adjust during testing. This will be removed from actual game.
             'game_money': 5000,         # money at the beginning of the game
@@ -42,7 +51,10 @@ def game_setup():
             'random_luck': 0.05,        # the possibility of benefiting from random functions
             'criminal_headstart': 4,    # the criminal's head start at the start of the game
             'criminal_time': 3,         # the time interval at which the criminal flies to the next location
-            'difficulty': 'easy'        # difficulty for quiz questions
+            'difficulty': 'easy',       # difficulty for quiz questions
+            'talk_to_chief': False,     # have we talked to security chief at current airport or not? True=yes, False=no
+            'previous_quiz_answer':bool,# did we answer right or wrong on previous quiz question? True=right, False=wrong
+            'clue_solved': False        # have we solved a clue at current airport? True=yes, False=no
         }
     }
 
@@ -60,24 +72,24 @@ def game_setup():
         difficulty_input = str(input("Choose difficulty of the game: 'E' = Easy, 'N' = Normal, 'H' = Hard: "))
 
         if difficulty_input.upper() in difficulty_settings:
-            game_parameters = difficulty_settings[difficulty_input.upper()] # stores the parameters ​​of the difficulty selected by the user 
+            game_dict = difficulty_settings[difficulty_input.upper()] # stores the parameters ​​of the difficulty selected by the user 
             break # break out from loop when valid input is given
         else:
             print("You entered an invalid input.")
 
 
     # Add screen_name and starting location to dictionary that is returned after
-    game_parameters["screen_name"] = screen_name # adding player's name to dictionary
-    game_parameters["player_location"] = result["ident"]
+    game_dict["screen_name"] = screen_name # adding player's name to dictionary
+    game_dict["player_location"] = result["ident"]
     
-    return game_parameters # return dictionary with parameters and screen name
+    return game_dict # return dictionary with parameters and screen name
 
 
 
 # Main block for testing
 if __name__ == "__main__":
-    game_parameters = game_setup()
-    print(game_parameters)
-    print(f"You have {game_parameters["game_money"]}€ in your bank account. ")
-    print(f"Screen name: {game_parameters["screen_name"]}")
-    print(f"You're at an airport with ICAO code: {game_parameters["player_location"]}")
+    game_dict = game_setup()
+    print(game_dict)
+    print(f"You have {game_dict["game_money"]}€ in your bank account. ")
+    print(f"Screen name: {game_dict["screen_name"]}")
+    print(f"You're at an airport with ICAO code: {game_dict["player_location"]}")
