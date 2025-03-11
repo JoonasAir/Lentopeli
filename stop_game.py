20dfc8f6906cf1ee9aeeabb7b7b50e6287669ba0
talvim104 <Tarya.Talvinen@metropolia.fi>
2025-03-11 14:40:36 +0200

    edit

--- stop_game.py
+++ stop_game.py
@@ -1,17 +1,21 @@
+def stop_game(game_dict:dict):
+
+
+    if game_dict["criminal_caught"]: # olemme saaneet rikollisen kiinni
+        print("Saat rikollisen kiinni. Voitit pelin! Peli päättyy!")
+        return True
 
 
 
-
-if game_dict["criminal_caught"]: # olemme saaneet rikollisen kiinni
-    pass
-
-
-
-if not game_dict["game_money"] >= game_dict["flight_price"]: #rahat loppu
-    pass
+    elif not game_dict["game_money"] >= game_dict["flight_price"]: #rahat loppu
+        print("Sinulla ei ole enää varaa lentolippuun. Hävisit pelin.")
+        return True
 
 
 
-if not game_dict["time_left_bool"]: # aika loppu
-    pass
+    elif not game_dict["time_left_bool"]: # aika loppu
+        print("Aika on loppunut. Hävisit pelin")
+        return True
+
+
 
