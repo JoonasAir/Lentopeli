5c6c425870022d7357de82be2535e8f5aa5c6e5f
talvim104 <Tarya.Talvinen@metropolia.fi>
2025-03-11 11:55:58 +0200

    add

---
+++ reduce_money.py
@@ -0,0 +1,20 @@
+
+def reduce_money(game_dict):
+
+    flight_cost = game_dict["flight_price"]
+
+#Tarkistaa käyttäjän rahaa
+    if game_dict["game_money"] < flight_cost:
+       print("You don't have enough money to buy a ticket. Game over!")
+       return game_dict
+
+#Vähentää lentolipun hinta
+    game_dict["game_money"] = game_dict["game_money"] - game_dict["flight_price"]
+
+#Näyttää kuinka paljon rahaa on vielä jäljellä
+    print(f"You took a flight. Remaining money: {game_dict['game_money']}€.")
+
+    return game_dict
+
+if __name__ == "__main__":
+    pass
