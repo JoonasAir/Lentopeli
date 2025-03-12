383e86928530084bd5390ff7c641effc56583f3d
Eeli Kiuru <eeli.kiuru@metropolia.fi>
2025-03-12 11:02:17 +0200

    y

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import cProfile
 
 if __name__ == "__main__":
     # Generate the call tree starting from the main function
-    profiler = cProfile.Profile()
-    profiler.enable()
+    # profiler = cProfile.Profile()
+    # profiler.enable()
 
     main_menu()
 
-    profiler.disable()
-    profiler.dump_stats('my_program.prof')
+    # profiler.disable()
+    # profiler.dump_stats('my_program.prof')
