ae7c8f7b7e343faf5d4b8cea867a3dba5f00eaa1
Eeli Kiuru <eeli.kiuru@metropolia.fi>
2025-03-12 15:35:23 +0200

    bug fix

--- questions.py
+++ questions.py
@@ -202,7 +202,10 @@ def quiz_icao(ask_question_bool:bool, game_dict:dict):
         sql = "SELECT location FROM criminal WHERE visited = 0 LIMIT 1;"
         cursor.execute(sql)
         result = cursor.fetchone()
-        game_dict["next_location"] = result[0]
+        if type(result) == tuple: # if the sql query returns a value
+            game_dict["next_location"] = result[0]
+        else: # if there is no locations left to visit
+            game_dict["criminal_caught"] = True
 
     elif ask_question_bool == False:# we'll get wrong ICAO-code
         sql = f"SELECT ident FROM airport WHERE continent = 'EU' AND type = 'large_airport' AND ident NOT IN (SELECT location FROM criminal) AND ident != '{player_location}' ORDER BY RAND() LIMIT 1;"
