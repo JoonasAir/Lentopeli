de9b44d07e7759a5a7509fc45ff2d4f1ea383e83
Eeli Kiuru <eeli.kiuru@metropolia.fi>
2025-05-03 20:43:24 +0300

    up

--- python/main.py
+++ python/main.py
@@ -3,12 +3,13 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from game_setup import game_setup
 from game_parameters import game_parameters
+from airport_menu import airport_menu_input
 from questions import get_questions
 from stop_game import stop_game
 import json
 
 app = Flask(__name__)
-cors = CORS(app)
+CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 
@@ -29,7 +30,7 @@ def leaderboard():
     return jsonify(results)
     
 
-    
+
 # create game_dict and return to game.js
 @app.route("/gameSetup", methods=['POST'])
 def gameSetup():
@@ -41,14 +42,19 @@ def gameSetup():
     return jsonify({"message": "Game setup received successfully", "data": game_dict}), 200
 
 
+
 @app.route("/flyto", methods=['PUT'])
 def get_location():
-    data = request.json["data"]
+    data = request.json
 
     cursor1 = mysql_connection.cursor()
     cursor2 = mysql_connection.cursor()
-    sqlfrom = f"SELECT latitude_deg, longitude_deg FROM airport WHERE ident='{data["player_location"]}'"
     sqlto = "SELECT latitude_deg, longitude_deg FROM airport WHERE ident='EHAM'"
+    try: 
+        sqlfrom = f"SELECT latitude_deg, longitude_deg FROM airport WHERE ident='{data["data"]["player_location"]}'"
+    except:
+        sqlfrom = f"SELECT latitude_deg, longitude_deg FROM airport WHERE ident='{data["player_location"]}'"
+
 
     cursor1.execute(sqlfrom)
     flyfrom = cursor1.fetchall()
@@ -67,10 +73,12 @@ def get_location():
     return jsonify(result)
 
 
+
 @app.route('/stopGame', methods=['POST'])
 def stopGame():
     data = request.json
-    returnValue = stop_game(data["data"])
+    
+    returnValue = stop_game(data["value"])
 
     if returnValue:
         return jsonify({"message": "Game ends now", "value": True}), 200
@@ -79,8 +87,20 @@ def stopGame():
 
 
 
+@app.route('/airportOptions', methods=['POST'])
+def airportOptions():
+    game_dict = request.json
+    try:
+        game_dict = game_dict["data"]
+    except:
+        pass
+    finally:
+        game_dict = airport_menu_input(game_dict)
 
-
+    try:
+        return jsonify({'message':"Airport menu options returned", 'value': game_dict["data"]})
+    except:
+        return jsonify({'message':"Airport menu options returned", 'value': game_dict})
 
 
 
